9c50021871b369ca81573ee043ef60e952ec8d23
Juan Rodriguez <juan.antonio.rodriguez.rendon@uabc.edu.mx>
2022-08-18 00:38:45 -0700

    YA ESTA EL CODIGO.

--- src/Inventario.java
+++ src/Inventario.java
@@ -8,12 +8,12 @@ public class Inventario {
     ArrayList<Producto> caducos;
 
     public Inventario(){
-        productos = new ArrayList();
-        ventas = new ArrayList();
-        caducos = new ArrayList();
+        productos = new ArrayList<>();
+        ventas = new ArrayList<>();
+        caducos = new ArrayList<>();
     }
     public int buscarPorID(int idB) {
-        int aux=1;
+        int aux=0;
         for (int i=0; i<productos.size();i++) {
             if (idB == productos.get(i).getId()) {
                 aux = i;
@@ -45,8 +45,8 @@ public class Inventario {
     public void darAlta(Producto p){
         productos.add(p);
     }
-    public void darBaja(int id, boolean motivo){
-       int idBuscar = buscarPorID(id);
+    public void darBaja(int x, boolean motivo){
+       int idBuscar = buscarPorID(x);
        if(motivo){
            ventas.add(productos.get(idBuscar));
            productos.get(idBuscar).setCantidad(productos.get(idBuscar).getCantidad()-1);
--- src/PruebaDeMetodos.java
+++ src/PruebaDeMetodos.java
@@ -1,17 +1,11 @@
+import java.util.ArrayList;
+
 public class PruebaDeMetodos {
     public static void main(String[] args) {
         Producto prueba = new Producto("Servilletas","Servilletas para casa","Charmin",40,1,4564,30,60);
-
         Inventario invP = new Inventario();
-        int aux=0;
-        for (int i=0; i< 2;i++) {
-            if (4564 == prueba.getId()) {
-                aux = 1;
-                break;
-            }
-        }
+        invP.darAlta(prueba);
         System.out.println(prueba.getId());
-        System.out.println(aux);
-
+        System.out.println(invP.buscarPorID(prueba.getId()));
     }
 }
